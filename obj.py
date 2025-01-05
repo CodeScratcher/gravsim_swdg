c513231903f38576109a423e7b6db78494df83bc
afisher <bb8andr2d2forlife@gmail.com>
2025-01-05 14:25:58 -0500

    objects should work

--- obj.py
+++ obj.py
@@ -1,15 +1,30 @@
 import numpy as np
 
-class MyClass:
-    def __init__(self, position, velocity):
+G = 6.67430e-11
+
+class GravityObject:
+    def __init__(self, position, velocity, mass):
         self.position = position
         self.velocity = velocity
+        self.mass = mass
         self.acceleration = np.array([0, 0])
     
-    def update(delta):
-        self.velocity += acceleration * delta * 0.5
-        self.position += velocity * delta
+    def update(objs, delta):
+        velocity += acceleration * delta * 0.5
+        position += velocity * delta
         
         # update gravity and acceleration here
+        acceleration = applyGravity(objs) / mass
 
-        self.velocity += acceleration * delta * 0.5
+        velocity += acceleration * delta * 0.5
+
+    def applyGravity(objs):
+        forces = np.array([0, 0])
+        
+        for obj in objs:
+            if not (obj.position == this).all():
+                pull = G * mass * obj.mass / np.linalg.norm(obj.position - position) ** 2
+                force = ((obj.position - position) / np.linalg.norm(obj.position - position)) * pull
+                forces += force
+        
+        return forces
